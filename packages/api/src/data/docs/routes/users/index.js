63f6f3b9812d4e374a09170ac0b26aed2c524b5e
Tiago Ribeiro <tiago.d.ribeiro@hotmail.com>
2023-11-27 10:49:45 +0000

    Added swagger docs to 2 routes

--- packages/api/src/data/docs/routes/users/index.js
+++ packages/api/src/data/docs/routes/users/index.js
@@ -9,7 +9,17 @@ const getLoggedUser = YAML.parse(
 	fs.readFileSync("./src/data/docs/routes/users/get_logged_user.yml", "utf8"),
 );
 
+const getUserProfile = YAML.parse(
+	fs.readFileSync("./src/data/docs/routes/users/get_user_profile.yml", "utf8"),
+);
+
+const updateUserProfile = YAML.parse(
+	fs.readFileSync("./src/data/docs/routes/users/patch_update_user_profile.yml", "utf8"),
+);
+
 module.exports = {
 	patchVerifyUser,
 	getLoggedUser,
+	getUserProfile,
+	updateUserProfile,
 };
