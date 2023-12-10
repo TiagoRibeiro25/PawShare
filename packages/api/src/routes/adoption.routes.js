7f9b07c9c629202092f52b22f91e5ba7bc97b827
pedromst2000 <40210465@esmad.ipp.pt>
2023-12-10 16:00:01 +0000

    added the get detail adoption route

---
+++ packages/api/src/routes/adoption.routes.js
@@ -0,0 +1,17 @@
+const { Router } = require("express");
+const validators = require("../validators");
+const controllers = require("../controllers");
+const middlewares = require("../middlewares");
+
+const router = Router();
+
+// Get detail of one adoption
+router.get('/:id',
+    validators.adoption.getAdoptionDetail(),
+    validators.validateResult,
+    middlewares.validateTokens,
+    controllers.adoption.getAdoptionDetail
+    )
+
+
+module.exports = router;
