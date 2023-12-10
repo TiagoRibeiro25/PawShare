460e88ca89936f5a671b4ab7c20deaed008f948f
pedromst2000 <40210465@esmad.ipp.pt>
2023-12-10 15:52:37 +0000

    add adoption folder controllers

--- packages/api/src/controllers/index.js
+++ packages/api/src/controllers/index.js
@@ -1,5 +1,6 @@
 const users = require("./users");
+const adoption = require("./adoption");
 const auth = require("./auth");
 const cronjob = require("./cronjob");
 
-module.exports = { users, auth, cronjob };
+module.exports = { users, adoption, auth, cronjob };
