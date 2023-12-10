4d5439c09e2d650e3a02c0e68719f5d62583ee51
pedromst2000 <40210465@esmad.ipp.pt>
2023-12-10 16:07:33 +0000

    add get adoption detail validator

---
+++ packages/api/src/validators/adoption/getAdoptionDetail.validator.js
@@ -0,0 +1,21 @@
+const {param} = require("express-validator");
+
+/**
+ * Returns an array of validation rules for the get adoption detail endpoint.
+ * @returns {Array} An array of validation rules
+ */
+
+function validator(){
+    return [
+        param('id').isInt().withMessage('Invalid adoption id'),
+        param('id').custom(value => {
+            if(value <= 0){
+                throw new Error('Invalid integer for adoption id');
+            }
+            return true;
+        })
+    ]
+}
+
+
+module.exports = validator;
