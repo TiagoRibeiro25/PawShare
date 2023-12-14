839d30bbb3c7ddac556a9a9347cf1ed5bb101fec
Tiago Ribeiro <tiago.d.ribeiro@hotmail.com>
2023-12-14 14:27:34 +0000

    Refactor: pagination default values config

---
+++ packages/api/src/config/pagination.config.js
@@ -0,0 +1,32 @@
+const config = {
+	adoptions: {
+		feed: {
+			defaultPage: 1,
+			defaultLimit: 6,
+		},
+		requested: {
+			defaultPage: 1,
+			defaultLimit: 20,
+		},
+		created: {
+			defaultPage: 1,
+			defaultLimit: 20,
+		},
+	},
+	sitting: {
+		feed: {
+			defaultPage: 1,
+			defaultLimit: 6,
+		},
+		requested: {
+			defaultPage: 1,
+			defaultLimit: 20,
+		},
+		created: {
+			defaultPage: 1,
+			defaultLimit: 20,
+		},
+	},
+};
+
+module.exports = config;
