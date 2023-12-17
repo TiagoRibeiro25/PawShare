c22a81df5b0cb4466befb4005d3310fff8e6c419
Tiago Ribeiro <tiago.d.ribeiro@hotmail.com>
2023-12-17 00:30:07 +0000

    chore: added more reset db data

--- packages/api/src/data/db/index.js
+++ packages/api/src/data/db/index.js
@@ -1,9 +1,13 @@
 const users = require("./users");
 const animals = require("./animals");
 const adoptions = require("./adoptions");
+const usersList = require("./usersList");
+const sittings = require("./sittings");
 
 module.exports = {
 	users,
 	animals,
 	adoptions,
+	sittings,
+	usersList,
 };
---
+++ packages/api/src/data/db/sittings.js
@@ -0,0 +1,13 @@
+// TODO: Add more data
+
+module.exports = [
+	{
+		owner_id: 1,
+		animal_id: 1,
+		email_contact: "john.doe@pawshare.com",
+		phone_contact: "123456789",
+		notes: "['Lorem ipsum dolor sit amet, consectetur adipiscing elit.']",
+		city: "Porto",
+		coins: 200,
+	},
+];
