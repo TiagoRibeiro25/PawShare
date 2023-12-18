864c01d623522371063a944c3249ae764ac050f5
Tiago Ribeiro <tiago.d.ribeiro@hotmail.com>
2023-12-18 19:24:20 +0000

    docs: Updated swagger routes

--- packages/api/src/config/docs.config.js
+++ packages/api/src/config/docs.config.js
@@ -21,9 +21,9 @@ const swaggerDefinition = {
 		"/users/me": { get: routeDocs.users.getLoggedUser },
 		"/users/{id}": { get: routeDocs.users.getUserProfile },
 		"/users": { patch: routeDocs.users.updateUserProfile },
-		"/animals/:id": { get: routeDocs.animals.getAnimalDetail },
+		"/animals/{id}": { get: routeDocs.animals.getAnimalDetail },
 		"/store/coins": { patch: routeDocs.store.patchHandleCoins },
-		"/store/buy/:itemId": { patch: routeDocs.store.patchBuyItem },
+		"/store/buy/{itemId}": { patch: routeDocs.store.patchBuyItem },
 		"/reviews": { post: routeDocs.review.postAddReview },
 		"/adoption": {
 			get: routeDocs.adoption.getAdoptionFeed,
