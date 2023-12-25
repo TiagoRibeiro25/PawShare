fe27156b485fd90b0233eac966dcdab177f23ea0
Pedro Teixeira <40210465@esmad.ipp.pt>
2023-12-25 15:15:16 +0000

    Feat:  Delete animal adoption

--- packages/api/src/controllers/adoption/index.js
+++ packages/api/src/controllers/adoption/index.js
@@ -1,13 +1,15 @@
 const getAdoptionDetail = require("./adoptionDetail.controller");
 const getAdoptionsFeed = require("./getAdoptionsFeed.controller");
+const getRequestedAdoptions = require("./getRequestedAdoptions.controller");
 const addAnimalAdoption = require("./addAnimalAdoption.controller");
 const addCandidateAdoption = require("./addCandidateAdoption.controller");
-const getRequestedAdoptions = require("./getRequestedAdoptions.controller");
+const deleteAnimalAdoption = require("./deleteAnimalAdoption.controller");
 
 module.exports = {
 	getAdoptionDetail,
 	getAdoptionsFeed,
+	getRequestedAdoptions,
 	addAnimalAdoption,
 	addCandidateAdoption,
-	getRequestedAdoptions,
+	deleteAnimalAdoption,
 };
--- packages/api/src/validators/adoption/index.js
+++ packages/api/src/validators/adoption/index.js
@@ -1,13 +1,15 @@
 const getAdoptionDetail = require("./getAdoptionDetail.validator");
 const getAdoptionsFeed = require("./getAdoptionsFeed.validator");
+const getRequestedAdoptions = require("./getRequestedAdoptions.validator");
 const addAnimalAdoption = require("./addAnimalAdoption.validator");
 const addCandidateAdoption = require("./addCandidateAdoption.validator");
-const getRequestedAdoptions = require("./getRequestedAdoptions.validator");
+const deleteAnimalAdoption = require("./deleteAnimalAdoption.validator");
 
 module.exports = {
 	getAdoptionDetail,
 	getAdoptionsFeed,
+	getRequestedAdoptions,
 	addAnimalAdoption,
 	addCandidateAdoption,
-	getRequestedAdoptions,
+	deleteAnimalAdoption,
 };
