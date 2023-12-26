32767345c05372935982d7ad7c5d99e44f6c2900
Pedro Teixeira <40210465@esmad.ipp.pt>
2023-12-26 16:26:16 +0000

    Feat: Delete animal sitting

--- packages/api/src/controllers/sitting/index.js
+++ packages/api/src/controllers/sitting/index.js
@@ -2,10 +2,12 @@ const getSittingsFeed = require("./getSittingsFeed.controller");
 const getRequestedSittings = require("./getRequestedSittings.controller");
 const getSittingDetail = require("./getSittingDetail.controller");
 const addAnimalSitting = require("./addAnimalSitting.controller");
+const deleteAnimalSitting = require("./deleteAnimalSitting.controller");
 
 module.exports = {
 	getSittingsFeed,
 	getRequestedSittings,
 	getSittingDetail,
 	addAnimalSitting,
+	deleteAnimalSitting,
 };
