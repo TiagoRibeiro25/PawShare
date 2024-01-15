8c3812315a5ecd02409b676a4389c94654294d79
Tiago Ribeiro <tiago.d.ribeiro@hotmail.com>
2024-01-15 20:27:18 +0000

    feat: Added animation to the edit button

--- packages/app/src/components/EditButton/index.tsx
+++ packages/app/src/components/EditButton/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import EditIcon from '../../assets/svg/edit.svg';
+import AnimatedScreen from '../AnimatedScreen';
 import Button from '../Button';
 import Icon from '../Icon';
 import { Props } from './types';
 
 const EditButton: React.FC<Props> = ({ onPress }): React.JSX.Element => {
 	return (
-		<Button className="absolute p-3 right-3 bottom-4 bg-accent-500 " onPress={onPress}>
-			<Icon icon={EditIcon} />
-		</Button>
+		<AnimatedScreen animation="SlideInFromBottom">
+			<Button className="absolute p-3 right-3 bottom-4 bg-accent-500 " onPress={onPress}>
+				<Icon icon={EditIcon} />
+			</Button>
+		</AnimatedScreen>
 	);
 };
 
